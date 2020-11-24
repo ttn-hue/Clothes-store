dc4e3e27b2de128cb9947a371feb830e3dceae50
tyngutha <tyngutha@gmail.com>
2020-11-23 23:23:58 -0500

    finish shoppingcart

--- singleproduct.php
+++ singleproduct.php
@@ -8,7 +8,7 @@
     <section class="products">
         <div class="product-card">
             <div class="product-image">
-             <img src="image/product-1.jpg"> 
+             <img src="image/product-1.jpg">
             </div>
             <div class="product-info">
                 <p class="title">Dress</p>
@@ -25,23 +25,23 @@
                 <p class="text-review">25 Reviews</p>
             </div></br></br>
                 <p>Samsa was a travelling salesman - and above it there hung a picture that he had recently cut out and illustrated magazine and housed in a nice, glided frame.</p></br>
-                
+
                 <div class="div-size">
                 <p class="size">Size</p>
                 <p class="required">(required)</p></br></br>
                  </div>
-                 
+
                 <select class="select">
                     <option id="s">SMALL</option>
                     <option value="m">MEDIUM</option>
                     <option value="b">BIG</option>
-                    
+
                 </select></br></br>
                 <div class="div-quantity">
                 <input type="text" id="quantity" name="quantity" placeholder="1" maxlength="2">
                 <button class="button-cart">Add item to cart</button>
                 </div></br>
-               
+
                 <p class="category">Category</p>
                 <p class="descriptions">Top and blouses</p> </br>
                 <p class="tags">Tags</p>
