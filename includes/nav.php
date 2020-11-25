e4286da87c2e0bbf313ffcb84956502d19e14f78
Kenneth <kenm.riv@gmail.com>
2020-11-25 16:28:02 -0500

    feature/insert was added insert user to nav menu

--- includes/nav.php
+++ includes/nav.php
@@ -16,7 +16,7 @@
                <li><a href="insertuser.php">Users</a></li>
                 <li><a href="logout.php">Log out</a></li>
                 <li><a href="#"><i class="fa fa-search fa-ms"></i></a></li>
-                <li><a href="#"><i class="fa fa-user fa-ms"></i></a></li>
+                <li><a href="insertuser.php"><i class="fa fa-user fa-ms"></i></a></li>
                 <li><a href="#"><i class="fa fa-shopping-cart fa-ms"></i></a></li>
             </ul>
         </div>
