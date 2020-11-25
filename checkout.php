2af9355b9147cd61cc8550ac0e20b612ff25358f
tyngutha <tyngutha@gmail.com>
2020-11-25 17:42:26 -0500

    added tax and total fixing

--- checkout.php
+++ checkout.php
@@ -1,34 +1,35 @@
 <?php
 session_start();
 include('includes/db_connection.php');
-// if(isset($_SESSION['email']) && ($_SESSION['role'] == "admin" || $_SESSION['role'] == "manager" || $_SESSION['role'] == "user")){
-    if(true){
+if(isset($_SESSION['email'])){ // authentification
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <?php include("includes/head.php") ?>
+    <script>
+    $(document).ready(function(){
+            // update tax value by select province
+            $('#proviceSelect').on('change', function(){
+                var province = $('#proviceSelect option:selected').val();
+                var subtotal = $('#subtotal').text().replace('$', '');
 
-        ?>
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-        <?php include("includes/head.php") ?>
-        <script>
-        $(document).ready(function(){
-                // update tax value by select province
-                $('#proviceSelect').on('change', function(){
-                    var province = $('#proviceSelect').val();
-                    var subtotal = $('#subtotal').text().replace('$', '');
-                    subtotal = parseFloat(subtotal);
-                   // ajax call
-                    $.ajax({
-                        type : 'POST',
-                        url : 'ajaxtaxprocess.php', // file for tax processing
-                        data : {
-                            province : province,
-                            subtotal : subtotal,
-                            //shopingcart : shoppingcart
-                        },
-                        success : function(result){
-                            $('#tax').html(result);
-                        },
-                    })
+                // ajax call
+                $.ajax({
+                    type : 'POST',
+                    url : 'ajaxtaxprocess.php', // file for tax processing
+                    data : {
+                        province : province,
+                        subtotal : subtotal,
+                    },
+                    success : function(result){
+                        $('#tax').html(`$${result}`);
+                        var subtotal = $('#subtotal').text().replace('$', '');
+                        var subtotal = parseFloat(subtotal);
+                        var tax = parseFloat(result);
+                        var total = subtotal + tax;
+
+                        $('#total').html(`$${total}`);
 
                     }
                 })
@@ -56,31 +57,30 @@ include('includes/db_connection.php');
 
                 // console.log(subtotal + " " + tax + " " + total + " " + province + " " + city + " " + address + " " + postcode + " " + phone + " " + email + " " + lastname + " " + firstname + " " + cardNumber + " " + expiryMonth + " " + expiryYear);
 
-                    // ajax call
-                    $.ajax({
-                        type    : 'POST',
-                        url     : 'ajaxcheckoutprocess.php',
-                        data    : {
-                            'firstname' : firstname,
-                            'lastname' : lastname,
-                            'email' : email,
-                            'phone' : phone,
-                            'postcode' : postcode,
-                            'address' : address,
-                            'city' : city,
-                            'province' : province,
-                            'cardNumber' : cardNumber,
-                            'expiryMonth' : expiryMonth,
-                            'expiryYear' : expiryYear,
-                            'subtotal' : subtotal,
-                            'tax' : tax,
-                            'total' : total
-                        },
-                        success : function(result) {
-                            // window.location.href = 'receipt.php';
-                            $('#formResult').html(result);
-                         }
-                    });
+                // ajax call
+                $.ajax({
+                    type    : 'POST',
+                    url     : 'ajaxcheckoutprocess.php',
+                    data    : {
+                        'firstname' : firstname,
+                        'lastname' : lastname,
+                        'email' : email,
+                        'phone' : phone,
+                        'postcode' : postcode,
+                        'address' : address,
+                        'city' : city,
+                        'province' : province,
+                        'cardNumber' : cardNumber,
+                        'expiryMonth' : expiryMonth,
+                        'expiryYear' : expiryYear,
+                        'subtotal' : subtotal,
+                        'tax' : tax,
+                        'total' : total
+                    },
+                    success : function(result) {
+                        localStorage.setItem('order_id', result);
+                        window.location.href = 'receipt.php';
+                    }
                 });
             });
         });
@@ -118,40 +118,6 @@ include('includes/db_connection.php');
                                         <label for="lastname">Last Name</label>
                                         <input type="text" id="lastname" placeholder="Last Name" name="lastname">
                                     </div>
-                                    <label for="email">Email</label>
-                                    <input type="email" id="email" name="email" placeholder="Email">
-                                    <label for="phone">Phone</label>
-                                    <input type="tel" name="phone" id="phone" placeholder="Phone">
-                                    <label for="postcode">Postcode</label>
-                                    <input type="text" name="postcode" id="postcode" placeholder="Postcode">
-                                    <label for="address">Address</label>
-                                    <input type="text" name="address" id="address" placeholder="Address">
-                                    <label for="city">City</label>
-                                    <input type="text" name="city" id="city" placeholder="City">
-                                    <label for="province">Province</label>
-                                    <select name="province" id="proviceSelect">
-                                         <option value="ON">Ontario</option>
-                                        <option value="MB">Manitoba</option>
-                                        <option value="BC">British Columbia</option>
-                                        <option value="QC">Quebec</option>
-                                        <option value="AB">Alberta</option>
-                                        <option value="SK">Saskatchewan</option>
-                                        <option value="NL">Newfoundland and Labrador</option>
-                                        <option value="PE">Prince Edward Island</option>
-                                        <option value="NS">Nova Scotia</option>
-                                        <option value="NU">Nunavut</option>
-                                        <option value="NT">Northwest</option>
-                                        <option value="YT">Yukon</option>
-                                    </select>
-                                </div>
-                                <div class="payment-details">
-                                    <h2>Payment Details</h2>
-                                    <label for="cartnumber">Card Number</label>
-                                    <input type="text" name="cartnumber" id="cartnumber" placeholder="Card Number">
-                                    <label for="expirymonth">Expiry Month</label>
-                                    <input type="text" name="expirymonth" id="expirymonth" placeholder="Expiry Month">
-                                    <label for="expiryyear">Expiry Year</label>
-                                    <input type="text" name="expiryyear" id="expiryyear" placeholder="Expiry Year">
                                 </div>
                                 <label for="email">Email</label>
                                 <input type="email" id="email" name="email" placeholder="Email">
